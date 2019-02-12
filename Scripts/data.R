0c380fb1402a57a3a431761165e5122c6338e1e0
Ida Riis Jensen <s161777@student.dtu.dk>
2019-02-12 14:40:54 +0100

    her

---
+++ Scripts/data.R
@@ -0,0 +1,10 @@
+rm(list = ls())
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+par(mar=c(3,3,2,1), mgp=c(2,0.7,0))
+
+
+## Loading data
+dat <- read.csv('../DistrictHeatingTestSample.csv', header=TRUE, sep = "\t")
+head(dat)
+str(dat)
+
