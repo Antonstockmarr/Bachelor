46ec711837db3dac5b2284245f0afff323744d7b
MikkelLaursen16 <47563587+MikkelLaursen16@users.noreply.github.com>
2019-02-15 10:28:49 +0100

    Update MikkelsHyggescript.R

--- Scripts/MikkelsHyggescript.R
+++ Scripts/MikkelsHyggescript.R
@@ -11,13 +11,13 @@ str(dat)
 ## Loading all data
 data.path = "../Watts_DistrictHeatingData_2018/"
 file.names <- dir(data.path, pattern =".csv")
-BigData <- vector(mode="list",length=length(file.names))
+DATA <- vector(mode="list",length=length(file.names))
 
 for(i in 1:length(file.names)){
-  df.temp <- read.table(paste(data.path,file.names[i], sep = ""),sep=";", stringsAsFactors=FALSE, header = TRUE)
-  if(dim(df.temp)[2]){
-    df.temp <- read.table(paste(data.path,file.names[i], sep = ""),sep="\t", stringsAsFactors=FALSE, header = TRUE)
+  df.temp <- read.csv(paste(data.path,file.names[i], sep = ""),sep=";", stringsAsFactors=FALSE, header = TRUE)
+  if(dim(df.temp)[2]==1){
+    df.temp <- read.csv(paste(data.path,file.names[i], sep = ""),sep="\t", stringsAsFactors=FALSE, header = TRUE)
   }
-  BigData[i] <- df.temp
+  DATA[[i]] <- df.temp
 }
 
