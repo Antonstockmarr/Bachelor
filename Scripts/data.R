135d14f4f94124c95e01be231c440c0579639933
Ida Riis Jensen <s161777@student.dtu.dk>
2019-02-15 13:11:50 +0100

    hej

--- Scripts/data.R
+++ Scripts/data.R
@@ -35,10 +35,8 @@ for(i in 1:n){
 # Reading weather data  
 weather <- read.table('../WeatherData_01-01-2018_09-05-2019.csv', sep="\t", stringsAsFactors=FALSE, header = TRUE, dec=',')
 str(weather)
-weather$IsHistoricalEstimated <- type.convert(weather$IsHistoricalEstimated, as.is = TRUE)
 
-Weather <- read.table('../WeatherData_01-01-2018_09-05-2019.csv', sep="\t", stringsAsFactors=FALSE, header = TRUE, dec=',')
-Weather$StartDateTime = strptime(Weather$StartDateTime,format='%d/%m/%Y %H.%M',tz = 'GMT')
-Weather$IsHistoricalEstimated=Weather$IsHistoricalEstimated=="True"
+weather$StartDateTime = strptime(weather$StartDateTime,format='%d/%m/%Y %H.%M',tz = 'GMT')
+weather$IsHistoricalEstimated=weather$IsHistoricalEstimated=="True"
 
 rm(i,n,file.names,data.path,dt.tmp,Datalengths)
