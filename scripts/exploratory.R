6e12b743b2455d4f7cc78b35666aba7d2494555f
Anton Stockmarr <s164170@student.dtu.dk>
2019-02-18 11:43:31 +0100

    wip

--- scripts/exploratory.R
+++ scripts/exploratory.R
@@ -3,3 +3,11 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 par(mar=c(3,3,2,1), mgp=c(2,0.7,0))
 
 source("data.R")
+
+test.data <- data[[1]]
+test.data$EndDateTime <- NULL
+test.data$StartDateTime <- 1:length(test.data$StartDateTime)
+
+pairs(test.data)
+
+plot(test.data$Flow[test.data$Flow<0.4] ~ test.data$StartDateTime[test.data$Flow<0.4])
