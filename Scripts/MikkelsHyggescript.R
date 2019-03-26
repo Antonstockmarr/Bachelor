e3cf26abc39e8951078a3206617f8352bb715a39
MikkelLaursen16 <47563587+MikkelLaursen16@users.noreply.github.com>
2019-03-26 15:34:47 +0100

    Update MikkelsHyggescript.R

--- Scripts/MikkelsHyggescript.R
+++ Scripts/MikkelsHyggescript.R
@@ -46,13 +46,74 @@ for(k in 1:n){
   plot(fitted.temp,plot.points,main=k)#,ylim=c(-1,.7))
 }
 
+alpha <- rep(0,n)
+j=1
+
+for(k in 1:n){
+  if (length(day.data[[k]]$Flow)>=365){
+  day.tmp <- day.weather[(day.weather$Date <= as.Date(EndDays[k],tz="GMT")),]
+  day.tmp <- day.tmp[day.tmp$Date >= as.Date(StartDays[k],tz="GMT"),] 
+  t <- day.tmp$Temperature
+  q <- day.data[[k]]$CoolingDegree*day.data[[k]]$Volume
+  
+  tmp.mu <- mean(q[t>=20])
+  tmp.sd <- sd(q[t>=20])
+  
+  if(tmp.sd!=0){
+    
+
+  
+  tmp.sd.dist <-(q-tmp.mu)/tmp.sd
+  
+  par(mfrow=c(1,2))
+  plot(t,tmp.sd.dist,main=k)#,xlim=c(5,25))
+  lines(t,rep((2),length(t)),col=2)
+  lines(t,rep((2),length(t)),col=2)
+  
+  pct.in.sd <- rep(0,length(min(t):(max(t)-1)))
+  
+  for (i in floor(min(t):(max(t)-1))){
+    tmp.q <- q[t<=(i+1)]
+    tmp.t <- t[t<=(i+1)]
+    tmp.q <- tmp.q[tmp.t>i]
+    tmp.t <- tmp.t[tmp.t>i]
+    
+    pct.in.sd[i-round(min(t))+1] <- (sum(tmp.q>=tmp.mu+2*tmp.sd)+sum(tmp.q<=tmp.mu-2*tmp.sd))/length(tmp.q)
+    
+  }
+  alpha[j]<-min(which(pct.in.sd<0.8))+floor(min(t)+1)
+  j=j+1
+  plot(min(t):(max(t)-1),pct.in.sd)
+  
+  }
+  
+  }
+}
+
+alpha<-alpha[alpha>0]
+
+hist(alpha)
+
+quantile(alpha, .15)
+
+
+day.no.summer<-day.data
+
+for (i in 1:n) {
+  day.tmp <- day.weather[(day.weather$Date <= as.Date(EndDays[i],tz="GMT")),]
+  day.tmp <- day.tmp[day.tmp$Date >= as.Date(StartDays[i],tz="GMT"),]
+  
+  tmp.nos<-day.data[[i]]
+  
+  tmp.nos<-tmp.nos[day.tmp$Temperature<=12,]
+  day.no.summer[[i]]<-tmp.nos
+}
 
 
 
 
 
-
-
+str(day.no.summer[[1]])
 
 
 
