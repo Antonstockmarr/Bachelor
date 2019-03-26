d250a664c41af7ab0339e0f5473b41e286c04700
MikkelLaursen16 <47563587+MikkelLaursen16@users.noreply.github.com>
2019-03-26 15:35:56 +0100

    Update SimpleRegression.R

--- scripts/SimpleRegression.R
+++ scripts/SimpleRegression.R
@@ -5,3 +5,14 @@ par(mar=c(3,3,2,1), mgp=c(2,0.7,0))
 source("data.R")
 
 # Define data set
+day.no.summer<-day.data
+
+for (i in 1:n) {
+  day.tmp <- day.weather[(day.weather$Date <= as.Date(EndDays[i],tz="GMT")),]
+  day.tmp <- day.tmp[day.tmp$Date >= as.Date(StartDays[i],tz="GMT"),]
+  
+  tmp.nos<-day.data[[i]]
+  
+  tmp.nos<-tmp.nos[day.tmp$Temperature<=12,]
+  day.no.summer[[i]]<-tmp.nos
+}
