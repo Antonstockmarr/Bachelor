4cb7b92db75232ed3b1cddfe11435367ad890bed
Ida Riis Jensen <s161777@student.dtu.dk>
2019-03-29 13:50:22 +0100

    her

--- scripts/SimpleRegression.R
+++ scripts/SimpleRegression.R
@@ -5,7 +5,7 @@ par(mar=c(3,3,2,1), mgp=c(2,0.7,0))
 source("data.R")
 
 
-model.data <- day.data
+model.data <- weatherCons
 for (i in 1:1) {
   day.tmp <- day.weather[(day.weather$Date <= as.Date(EndDays[i],tz="GMT")),]
   day.tmp <- day.tmp[day.tmp$Date >= as.Date(StartDays[i],tz="GMT"),]
@@ -16,11 +16,11 @@ for (i in 1:1) {
   model.data[[i]] <- model.tmp
   day.tmp <- day.tmp[day.tmp$Temperature <= 12,]
   
-  lm.simple <- lm((model.data[[i]]$CoolingDegree*model.data[[i]]$Volume) ~ day.tmp$Temperature)
+  lm.simple <- lm(tmpcons ~ Temperature, data = model.tmp)
   # Checking model assumptions 
   par(mfrow = c(2,2))
   plot(lm.simple)
 }
-plot((model.data[[1]]$CoolingDegree*model.data[[1]]$Volume) ~ day.tmp$Temperature)
+plot(Consumption ~ Temperature, data = model.tmp)
 abline(lm.simple)
 
