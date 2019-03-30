22fae49f5437a03ad0d5ef1ad35a9d5a208bc0ba
Anton Stockmarr <s164170@student.dtu.dk>
2019-03-30 15:22:21 +0100

    .

--- scripts/MultipleRegression.R
+++ scripts/MultipleRegression.R
@@ -14,29 +14,31 @@ cores = detectCores()
 # Defining new data set where the summer period is left out
 model.data <- weatherCons
 lmMultiple <- vector(mode="list", length = n)
-
-for (i in 1:2) {
-  model.tmp <- model.data[[i]]
-  model.tmp <- model.tmp[model.tmp$Temperature <= 12,]
-
-  lmMultiple[[i]] <- stepP(lm(Consumption ~ Temperature*WindSpeed*WindDirection*SunHour*Condition*
-                     UltravioletIndex*MeanSeaLevelPressure+Holiday, data = model.tmp))
-}
-
-summary(lmMultiple[[1]]$object)
-plot(lmMultiple[[2]]$object)
-
-plot(Consumption~Temperature,data=model.tmp)
-lines(lmMultiple$object)
 # 
-# plot(lmMultiple[[1]])
-par(mfrow = c(2,2))
-plot(lm.multiple)
-
+# for (i in 1:2) {
+#   model.tmp <- model.data[[i]]
+#   model.tmp <- model.tmp[model.tmp$Temperature <= 12,]
+# 
+#   lmMultiple[[i]] <- stepP(lm(Consumption ~ Temperature*WindSpeed*WindDirection*SunHour*Condition*
+#                      UltravioletIndex*MeanSeaLevelPressure+Holiday, data = model.tmp))
+# }
+# 
+summary(lmMultiple[[5]]$object)
+plot(lmMultiple[[2]]$object)
+# 
+# plot(Consumption~Temperature,data=model.tmp)
+# lines(lmMultiple$object)
+# # 
+# # plot(lmMultiple[[1]])
+# par(mfrow = c(2,2))
+# plot(lm.multiple)
+# 
 c <- makeCluster(cores[1]-1)
 registerDoParallel(c)
 
-lmMultiple <- foreach(i=1:n) %dopar% {
+lmMultiple <- foreach(i=1:1) %dopar% {
+  model.tmp <- model.data[[i]]
+  model.tmp <- model.tmp[model.tmp$Temperature <= 12,]
   MultiModel <- stepP(lm(Consumption ~ Temperature*WindSpeed*WindDirection*SunHour*Condition*
                                 UltravioletIndex*MeanSeaLevelPressure+Holiday, data = model.tmp))
   MultiModel
