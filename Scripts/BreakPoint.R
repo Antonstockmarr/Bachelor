4fb22e33022a90a9d78f7c8f72c078a9cf3e60bc
MikkelLaursen16 <47563587+MikkelLaursen16@users.noreply.github.com>
2019-04-18 22:32:05 +0200

    Kommentarer

--- Scripts/BreakPoint.R
+++ Scripts/BreakPoint.R
@@ -1,8 +1,10 @@
 # Dette script kan kun k?res efter "source("data.R")"
 
+# Initialisere et alpha for hvert hus.
 alpha <- rep(0,n)
 j=1
 
+# Hvis der er mindst 1 års data, kigger vi på det hus.
 for(k in 1:n){
   if (length(day.data[[k]]$Flow)>=365){
     day.tmp <- day.weather[(day.weather$Date <= as.Date(EndDays[k],tz="GMT")),]
@@ -10,9 +12,11 @@ for(k in 1:n){
     t <- day.tmp$Temperature
     q <- day.data[[k]]$CoolingDegree*day.data[[k]]$Volume
     
+    #Vi antager normalfordeling i consumption på dage over 20 grader, og ser på resten af data i forhold til denne normalfordeling.
     tmp.mu <- mean(q[t>=20])
     tmp.sd <- sd(q[t>=20])
     
+    # Hvis der ikke er en standard deviation bruges data ikke.
     if(tmp.sd!=0){
       
       tmp.sd.dist <-(q-tmp.mu)/tmp.sd
@@ -24,6 +28,7 @@ for(k in 1:n){
       mtext(paste("House number ",k), outer = TRUE, cex = 1.5)
       pct.in.sd <- rep(0,length(min(t):(max(t)-1)))
       
+      # For hver grad tjekkes hvilken andel af datapunkterne der ligger indenfor 2 sd fra mean i normalfordelingen.
       for (i in floor(min(t):(max(t)-1))){
         tmp.q <- q[t<=(i+1)]
         tmp.t <- t[t<=(i+1)]
@@ -33,6 +38,7 @@ for(k in 1:n){
         pct.in.sd[i-round(min(t))+1] <- (sum(tmp.q>=tmp.mu+2*tmp.sd)+sum(tmp.q<=tmp.mu-2*tmp.sd))/length(tmp.q)
         
       }
+      # Alpha for det pågældende hus er den koldeste grad hvor 80% eller mere er indenfor 2 sd i normalfordelingen
       alpha[j]<-min(which(pct.in.sd<0.8))+floor(min(t)+1)
       plot(min(t):(max(t)-1),pct.in.sd,xlab='Temperature',ylab='Proportion inside interval',col=Wcol[2])
       lines(t,rep(0.8,length(t)),col=Wcol[4])
@@ -46,6 +52,7 @@ alpha<-alpha[alpha>0]
 
 hist(alpha)
 
+# Breakpointet bestemmes som 15% kvartilet af alphaerne.
 break.point<-as.numeric(quantile(alpha, .15))
 
 rm(alpha,i,j,pct.in.sd,q,t,tmp.mu,tmp.q,tmp.sd,tmp.sd.dist,tmp.t)
