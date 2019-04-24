c88befe068d0f1f8c2d4b918853ceb745fb31378
MikkelLaursen16 <47563587+MikkelLaursen16@users.noreply.github.com>
2019-04-24 10:57:09 +0200

    .

--- Scripts/BSplines.R
+++ Scripts/BSplines.R
@@ -4,5 +4,8 @@ BSplines <- function(wd)
 {
   library(pbs)
   wd = wd[order(wd)]
-  lasse = pbs(wd, df = NULL, knots = c(90,180,270), degree = 2, intercept = T, Boundary.knots = c(0,360))
+  wd2 <- wd
+  wd2[wd2<45] <- wd2[wd2<45]+360
+  #Bs = pbs(wd, df = NULL, knots = c(90,180,270), degree = 2, intercept = T, Boundary.knots = c(0,360))
+  Bs = pbs(wd2, df = NULL, knots = c(135, 225,315), degree = 2, intercept = T, Boundary.knots = c(45,405))
 }
