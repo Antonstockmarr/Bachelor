336f1ecdf2f48b1226a62bf92b88a81e1a378be3
Ida Riis Jensen <s161777@student.dtu.dk>
2019-04-26 14:22:23 +0200

    hnkl

--- scripts/MultipleRegression.R
+++ scripts/MultipleRegression.R
@@ -5,25 +5,23 @@ par(mar=c(3,3,2,1), mgp=c(2,0.7,0))
 source("data.R")
 source("stepP.R")
 source("BSplines.R")
-library(foreach)
-library(doParallel)
-
-# setup parallel
-cores = detectCores()
 
 # Defining new data set where the summer period is left out
 model.data <- weatherCons
+# Various attributes are removed 
 for (i in 1:n)
 {
   model.data[[i]]$Date <- NULL
   model.data[[i]]$PrecipitationProbability <- NULL
   model.data[[i]]$SunHour <- NULL
 }
-lmMultiple <- vector(mode="list", length = n)
 
+
+# Full regression model ---------------------------------------------------
+lmMultiple <- vector(mode="list", length = n)
 for (i in 1:n) {
   
-  print(paste('Modelling house ',i))
+  print(paste('Modeling house ',i))
   model.tmp <- model.data[[i]]
   model.tmp <- model.tmp[model.tmp$Temperature <= 12,]
   Splinebasis <- BSplines(model.tmp$WindDirection)
@@ -43,39 +41,16 @@ for (i in 1:n) {
 }
 
 
-# 
+# Investigating parameters from model
 summary(lmMultiple[[60]]$object)
 par(mfrow=c(2,2))
-
+# Checking model assumptions 
 for (i in 1:n)
 {
   plot(lmMultiple[[i]]$object)
 }
-# 
-# plot(Consumption~Temperature,data=model.tmp)
-# lines(lmMultiple$object)
-# # 
-# # plot(lmMultiple[[1]])
-# par(mfrow = c(2,2))
-# plot(lm.multiple)
-# 
-# plot(lmMultiple[[1]])
 
-c <- makeCluster(cores[1]-1)
-registerDoParallel(c)
-
-
-lmMultiple <- foreach(i=1:n) %dopar% {
-  print(paste('Modeling house ',i))
-  model.tmp <- model.data[[i]]
-  model.tmp <- model.tmp[model.tmp$Temperature <= 12,]
-  Splinebasis <- BSplines(model.tmp$WindDirection)
-  MultiModel <- stepP(lm(Consumption ~ Temperature*WindSpeed*Splinebasis+(.-UltravioletIndex)^2, data = model.tmp))
-  MultiModel
-}
-#stop cluster
-stopCluster(c)
-
+# Saving slopes and p-values
 modelListSlope <- vector(mode="list",length=n)
 modelListPval <- vector(mode="list",length=n)
 
