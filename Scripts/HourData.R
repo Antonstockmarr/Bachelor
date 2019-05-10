acf8f885be5bfe73e39ba11e6380fadb81ccfdea
Anton Stockmarr <s164170@student.dtu.dk>
2019-05-10 14:14:03 +0200

    Hour

--- Scripts/HourData.R
+++ Scripts/HourData.R
@@ -1,7 +1,13 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("data.r")
 HourData <- vector("list",length=n)
+rotate <- function(x) t(apply(x, 2, rev))
 
-
+Avgcons <- c(1:n)
+for (i in 1:n)
+{
+  Avgcons[i] <- mean(weatherCons[[i]]$Consumption,na.rm = TRUE)
+}
 
 for (i in 1:n)
 {
@@ -26,12 +32,37 @@ for (i in 1:n)
       tmp <- tmp[tmp$ObsTime >= tail(data[[i]]$ObsTime,1),]
       Attribute[j+2] <- tmp[(hour(data[[i]]$ObsTime) == j),k]
     }
-    HourTmp[[k-1]] <- Attribute
+    HourTmp[[k-1]] <- as.matrix(Attribute)
   }
   
-  HourData[[i]]<- data.frame(Consumption = Cons, Temperature = HourTmp[[1]], WindSpeed = HourTmp[[2]],
-                             WindDirection = HourTmp[[3]], SunHour = HourTmp[[4]], Condition = HourTmp[[5]],
-                             UltravioletIndex = HourTmp[[6]], MeanSeaLevelPressure = HourTmp[[7]], DewPoint = HourTmp[[8]],
-                             Humidity = HourTmp[[9]], PrecipitationProbability = HourTmp[[10]],
-                             IsHistoricalEstimated = HourTmp[[11]], Radiation = HourTmp[[12]])
+  HourData[[i]]<- list(Consumption = as.matrix(Cons), Temperature = HourTmp[[1]], WindSpeed = HourTmp[[2]],
+                            WindDirection = HourTmp[[3]], SunHour = HourTmp[[4]], Condition = HourTmp[[5]],
+                            UltravioletIndex = HourTmp[[6]], MeanSeaLevelPressure = HourTmp[[7]], DewPoint = HourTmp[[8]],
+                            Humidity = HourTmp[[9]], PrecipitationProbability = HourTmp[[10]],
+                            IsHistoricalEstimated = HourTmp[[11]], Radiation = HourTmp[[12]])
 }
+
+Houravg = matrix(c(rep(0,24*n)),nrow = 24) 
+for (i in 1:n)
+  {
+  
+  Houravg[,i] <- (sapply(HourData[[i]]$Consumption[HourData[[i]]$Temperature[-1]>=15],FUN=mean,na.rm = TRUE)[-1])/Avgcons[i]
+  }
+rownames(Houravg) <- c('00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23')
+colnames(Houravg) <- c(1:n)
+tt <- Houravg
+library('fields')
+
+image.plot(t(tt[rev(order(row.names(tt))),]), axes=FALSE, 
+           lab.breaks=NULL,main = 'Average consumption of all houses during the day')
+axis(2, at=seq(1+1/48,0-1/48, length=13), labels=c('00','02','04','06','08','10','12','14','16','18','20','22','24'), lwd=0.1, pos=-0.01,las=1)
+abline(h=c(seq(1,0, length=24)+1/48),lwd=0.75)
+
+tmp <-do.call(rbind,HourData[[1]]$Temperature)
+HourData[[1]]$Temperature <- tmp
+str(HourData[[1]])
+
+HourData[[1]]$Temperature['Date',80]
+
+
+tmp <- as.matrix(Attribute)
