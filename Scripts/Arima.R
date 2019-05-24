1cbda934c242d7ac274bcba3362bee8229a35e8e
MikkelLaursen16 <47563587+MikkelLaursen16@users.noreply.github.com>
2019-05-24 13:08:12 +0200

    Update Arima.R

--- Scripts/Arima.R
+++ Scripts/Arima.R
@@ -1,10 +1,14 @@
 library("forecast")
-A <- arima(weatherCons[[40]]$Consumption, order =c(1,1,1), seasonal = list(order = c(0,0,1), period = 24))
+for(i in 1:n){
+A <- arima(weatherCons[[i]]$Consumption, order =c(1,0,1), seasonal = list(order = c(0,0,1), period = 24),xreg=weatherCons[[i]]$Temperature)
 lagmax = 4*24
-
+print(i)
 plot(A$residuals)
 acf(A$residuals,panel.first = c(abline(v=(1:4)*24,col="red",lty=2)),lag.max=lagmax,main="")
+Sys.sleep(1.5)
 acf(A$residuals,"partial",lag.max = lagmax,panel.first = c(abline(v=(1:4)*24,col="red",lty=2)),main="")
+Sys.sleep(0.5)
+}
 par(mfrow=c(1,1))
 qqnorm(A$residuals)
 qqline(A$residuals)
