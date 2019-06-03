63316469af6a54611163111e1a9707223232af6a
MikkelLaursen16 <47563587+MikkelLaursen16@users.noreply.github.com>
2019-06-03 15:25:22 +0200

    .

---
+++ Scripts/Predictions.R
@@ -0,0 +1,77 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+if(!exists("n")){
+  source("data.R")
+}
+source("TrainTest.R")
+source("BSplines.R")
+
+# Daily predictions ----------------------------------
+
+# Defining data used for modeling
+model.data <- weatherCons
+# Various attributes are removed
+for (i in 1:n)
+{
+  model.data[[i]]$Date <- NULL
+  model.data[[i]]$PrecipitationProbability <- NULL
+  model.data[[i]]$SunHour <- NULL
+  model.data[[i]]$UltravioletIndex <- NULL
+  model.data[[i]]$Condition <- NULL
+}
+
+ttd<-TrainTest(model.data,31)
+
+lmMultipleNoP <- vector(mode = "list", length = n)
+
+par(mfrow = c(1,1))
+for (i in 1:n) {
+  print(paste('Modeling house ',i))
+  model.tmp <- ttd[[1]][[i]]
+  model.tmp <- model.tmp[model.tmp$Temperature <= 12,]
+  Splinebasis <- BSplines(model.tmp$WindDirection)
+  
+  tmp.wind <- Splinebasis*model.tmp$WindSpeed
+  
+  model.tmp$North <- tmp.wind[,3]
+  model.tmp$East <- tmp.wind[,4]
+  model.tmp$South <- tmp.wind[,1]
+  model.tmp$West <- tmp.wind[,2]
+  lmMultipleNoP[[i]] <- lm(Consumption ~ Temperature*(North + East + South + West)+
+                             Radiation, data = model.tmp)
+  newData = ttd[[2]][[i]]
+  Splinetest <- BSplines(newData$WindDirection)
+  test.wind <- Splinetest*newData$WindSpeed
+  newData$North <- test.wind[,3]
+  newData$East <- test.wind[,4]
+  newData$South <- test.wind[,1]
+  newData$West <- test.wind[,2]
+  newData$Consumption<-NULL
+  Pred<-data.frame(predict(object=lmMultipleNoP[[i]], newdata=newData, interval = "confidence", level = 0.95))
+  
+  plot(Pred$fit,type='l',ylim=range(Pred$lwr,Pred$upr),main=paste("hus: ",i))
+  lines(Pred$upr,lty=2)
+  lines(Pred$lwr,lty=2)
+  lines(ttd[[2]][[i]]$Consumption,lty=1,col=2)
+}
+
+# Hourly Predictions ----------------------------------------
+
+tth<-
+
+
+
+
+(o4<-arima(sqrt(tab$O3[train]), order=c(2,0,0),seasonal = (list(order=c(0,1,1),period=24))))
+po<-predict(o4,n.ahead=48,se.fit=TRUE)
+o.sd<-po$se
+o.pred<-po$pred
+
+o.low<-o.pred-2*o.sd
+o.high<-o.pred+2*o.sd
+plot(time[train[600:703]],tab$O3[train[600:703]],type="l",xlim=c(605,751),ylim = c(0,150), xlab = "Time", ylab = "O3 concentration")
+lines(time[test],tab$O3[test], col = 2)
+lines(time[test],(po$pred)^2, col = 3)
+lines(time[test],(o.low)^2, col = 3, lty = 2)
+lines(time[test],(o.high)^2, col = 3, lty = 2)
+legend("topleft", c("Data", "Prediction", "95% PI"), lty = c(1,1,2), col = c(2,3,3))
+
